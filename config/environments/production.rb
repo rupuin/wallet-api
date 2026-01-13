2f0223b6d454368164c3da455cbe8777f3c2670b
rupuin <tasignas@gmail.com>
2026-01-13 20:47:42 +0100

    chore: prod config update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    '
-    wallet-dashboard-flax.vercel.app',     # Allow requests from example.com
+    'wallet-dashboard-flax.vercel.app',     # Allow requests from example.com
     /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   ]
 
